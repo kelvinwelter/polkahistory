0cbf0fff01ddc7835b5b0ad0b67d2f91ead56ea1
Kelvin Welter <kelvinwelter.b@gmail.com>
2022-03-25 23:29:20 -0300

    chore: install chakra-ui + related libs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ChakraProvider } from '@chakra-ui/react';
 
 import App from './App';
 import './styles/global.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
