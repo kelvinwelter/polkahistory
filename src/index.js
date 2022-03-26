87093a9ada008b5f50b85a05074271e7970c865e
Kelvin Welter <kelvinwelter.b@gmail.com>
2022-03-26 19:09:10 -0300

    chore: install react-intl lib

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ChakraProvider } from '@chakra-ui/react';
+import { IntlProvider } from 'react-intl';
 
 import App from './App';
 import './styles/global.css';
+import English from './locales/en.json';
+import Portuguese from './locales/pt.json';
+
+const localLanguage = navigator.language;
+let applicationLanguage;
+if (localLanguage.startsWith('en')) {
+  applicationLanguage = English;
+} else if (localLanguage.startsWith('pt')) {
+  applicationLanguage = Portuguese;
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <IntlProvider locale={localLanguage} messages={applicationLanguage} >
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </IntlProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
