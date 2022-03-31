654c880dece130dd6e3777f597c1c21121dac244
Kelvin Welter <kelvinwelter.b@gmail.com>
2022-03-30 23:00:52 -0300

    chore: add kusama route

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import {
     Route,
 } from "react-router-dom";
 import PolkadotSearch from "./screens/PolkadotSearch";
+import KusamaSearch from "./screens/KusamaSearch";
 
 export default function Router() {
     return (
         <Routes>
             <Route path="/" element={<PolkadotSearch />} />
             <Route path="/polkadot" element={<PolkadotSearch />} />
+            <Route path="/kusama" element={<KusamaSearch />} />
         </Routes>
     )
 }
