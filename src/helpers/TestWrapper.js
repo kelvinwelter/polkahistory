40deeb66d62f14f83c493a86dd9463bc7cc26b09
Kelvin Welter <kelvinwelter.b@gmail.com>
2022-04-06 19:24:44 -0300

    chore: setup testing environment

---
+++ src/helpers/TestWrapper.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter } from "react-router-dom";
+import { IntlProvider } from 'react-intl';
+
+import English from '../locales/en.json';
+import theme from '../styles/theme';
+
+const localLanguage = navigator.language;
+const applicationLanguage = English;
+
+export default function TestWrapper(props) {
+  return (
+    <BrowserRouter>
+        <IntlProvider locale={localLanguage} messages={applicationLanguage} >
+            <ChakraProvider theme={theme}>
+                {props.children}
+            </ChakraProvider>
+        </IntlProvider>
+    </BrowserRouter>
+  );
+}
